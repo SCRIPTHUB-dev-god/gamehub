e0e01b9ec4b6c6af0bb94ce54c34c9dbc832331b
SCRIPTHUB-dev-god <faruqaldaffa069@gmail.com>
2026-06-06 08:29:30 +0700

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,12 +1,27 @@
-local StarterGui = game:GetService("StarterGui")
+local NotificationLibrary = {}
 
+local StarterGui = game:GetService("StarterGui")
 repeat task.wait() until StarterGui
 
-local function sendRobloxNotif(title, text, duration, icon)
+function NotificationLibrary.send(title, text, duration, icon)
 	StarterGui:SetCore("SendNotification", {
 		Title = title,
 		Text = text,
 		Duration = duration or 5,
-		Icon = icon or "rbxassetid://0" -- kosongkan atau ganti dengan asset id kamu
+		Icon = icon or "rbxassetid://0"
 	})
 end
+
+function NotificationLibrary.success(text, duration)
+	NotificationLibrary.send("Success!", text, duration or 5, "rbxassetid://6031071053")
+end
+
+function NotificationLibrary.error(text, duration)
+	NotificationLibrary.send("Error!", text, duration or 5, "rbxassetid://6031071054")
+end
+
+function NotificationLibrary.info(title, text, duration)
+	NotificationLibrary.send(title, text, duration or 5)
+end
+
+return NotificationLibrary
